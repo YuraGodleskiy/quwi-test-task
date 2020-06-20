270aec8755908f3fdd15616781c7606a6fa751d7
GodlevskiyYuriy <godlevskiy.yuriy@gmail.com>
2020-06-21 02:05:26 +0300

    add navbar + animation + animation fix

--- pages/index.vue
+++ pages/index.vue
@@ -3,15 +3,6 @@
     <span class="asd">
       index
     </span>
-    <button
-      v-if="$auth.loggedIn"
-      @click="logoutUser"
-    >
-      Logout
-    </button>
-    <nuxt-link v-else to="login">
-      Login
-    </nuxt-link>
   </div>
 </template>
 
@@ -20,16 +11,9 @@
 export default {
   middleware: 'authenticated',
   async created () {
-    console.log(this.$auth.loggedIn)
     if (this.$auth.loggedIn) {
       const { data } = await this.$axios.get('projects-manage/index')
       console.log(data)
-    }
-  },
-  methods: {
-    async logoutUser () {
-      await this.$auth.logout()
-      await this.$router.push('login')
     }
   }
 }
