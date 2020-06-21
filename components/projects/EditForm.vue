9065155e9f4ead3559b1a094f2d7cd3ef25aabb8
GodlevskiyYuriy <godlevskiy.yuriy@gmail.com>
2020-06-22 00:03:48 +0300

    add full edit form

--- components/projects/EditForm.vue
+++ components/projects/EditForm.vue
@@ -3,13 +3,50 @@
     <h4 class="form__title">
       Edit name of project
     </h4>
-    <img v-if="form.logo" :src="form.logo" alt="logo" class="form__logo">
+    <img v-if="form.logo_url" :src="form.logo_url" alt="logo" class="form__logo">
     <img v-else :src="defaultLogo" alt="logo" class="form__logo">
-    <input
-      v-model="form.name"
-      class="form__text-field"
-      type="text"
-    >
+    <label class="toggle-wrapper">
+      <div class="toggle">
+        <input v-model="form.is_active" class="toggle-state" disabled type="checkbox" name="check">
+        <div class="indicator" />
+      </div>
+      <div class="label-text">
+        {{ form.is_active ? 'Active' : 'Not active' }}
+      </div>
+    </label>
+    <div class="edit-form__input-wrapper">
+      <label for="name">Name:</label>
+      <input
+        id="name"
+        v-model="form.name"
+        class="form__text-field"
+        type="text"
+      >
+      <label for="spent_time_week">spent_time_week:</label>
+      <input
+        id="spent_time_week"
+        v-model="form.spent_time_week"
+        class="form__text-field"
+        type="text"
+        readonly
+      >
+      <label for="spent_time_week">spent_time_month:</label>
+      <input
+        id="spent_time_month"
+        v-model="form.spent_time_month"
+        class="form__text-field"
+        type="text"
+        readonly
+      >
+      <label for="spent_time_all">spent_time_all:</label>
+      <input
+        id="spent_time_all"
+        v-model="form.spent_time_all"
+        class="form__text-field"
+        type="text"
+        readonly
+      >
+    </div>
     <span v-if="notificationText" :style="{color: notificationColor}" class="form__notification">
       {{ notificationText }}
     </span>
@@ -35,10 +72,7 @@ export default {
     }
   },
   data: () => ({
-    form: {
-      logo: '',
-      name: ''
-    },
+    form: {},
     defaultLogo,
     notificationText: '',
     notificationColor: ''
@@ -51,10 +85,11 @@ export default {
   async created () {
     await this.getProjects()
     // eslint-disable-next-line camelcase
-    const { name, logo_url } = await this.projects.find(item => item.id === +this.id)
-    this.form.name = name
-    // eslint-disable-next-line camelcase
-    this.form.logo = logo_url
+    const item = await this.projects.find(item => item.id === +this.id)
+    this.form = { ...item }
+    // this.form.name = name
+    // // eslint-disable-next-line camelcase
+    // this.form.logo = logo_url
   },
   methods: {
     ...mapActions({
