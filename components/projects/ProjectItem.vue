54f11fa5ffb0f81bda6aaaad4564e9076b78ea4c
GodlevskiyYuriy <godlevskiy.yuriy@gmail.com>
2020-06-21 17:37:05 +0300

    add full project item layout + responsive

---
+++ components/projects/ProjectItem.vue
@@ -0,0 +1,83 @@
+<template>
+  <div class="project-item">
+    <img :src="logo ? logo : defaultLogo" :alt="title" class="project-item__logo">
+    <b class="project-item__title">
+      {{ title }}
+    </b>
+    <b :style="isActiveStyle" class="project-item__active-mark">
+      {{ isActiveRes }}
+    </b>
+    <div class="project-item__time-info">
+      <p>
+        <span>
+          time this week
+        </span>
+        <b>
+          {{ spentTimeWeek }}
+        </b>
+      </p>
+      <p>
+        <span>
+          this month
+        </span>
+        <b>
+          {{ spentTimeMonth }}
+        </b>
+      </p>
+      <p>
+        <span>
+          total
+        </span>
+        <b>
+          {{ spentTimeAll }}
+        </b>
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+import defaultLogo from '@/assets/images/general/defaultLogo.png'
+export default {
+  name: 'ProjectItem',
+  props: {
+    logo: {
+      type: String,
+      default: ''
+    },
+    title: {
+      type: String,
+      default: ''
+    },
+    isActive: {
+      type: Boolean,
+      default: false
+    },
+    spentTimeWeek: {
+      type: Number,
+      default: 0
+    },
+    spentTimeMonth: {
+      type: Number,
+      default: 0
+    },
+    spentTimeAll: {
+      type: Number,
+      default: 0
+    }
+  },
+  data: () => ({
+    defaultLogo
+  }),
+  computed: {
+    isActiveRes () {
+      return this.isActive ? 'Active' : 'Not active'
+    },
+    isActiveStyle () {
+      return {
+        color: this.isActive ? 'green' : 'red'
+      }
+    }
+  }
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,23 +1,32 @@
 <template>
-  <div>
-    <span class="asd">
-      index
-    </span>
-  </div>
+  <section class="projects-section">
+    <ProjectItem
+      v-for="item in projects"
+      :logo="item.logo_url"
+      :title="item.name"
+      :is-active="!!item.is_active"
+      :spent-time-week="item.spent_time_week"
+      :spent-time-month="item.spent_time_month"
+      :spent-time-all="item.spent_time_all"
+    />
+  </section>
 </template>
 
 <script>
-
+import ProjectItem from '@/components/projects/ProjectItem'
 export default {
   middleware: 'authenticated',
-  async created () {
-    if (this.$auth.loggedIn) {
-      const { data } = await this.$axios.get('projects-manage/index')
-      console.log(data)
+  components: {
+    ProjectItem
+  },
+  data: () => ({
+    projects: []
+  }),
+  async asyncData ({ $axios }) {
+    const { data } = await $axios.get('projects-manage/index')
+    return {
+      projects: data.projects
     }
   }
 }
 </script>
-
-<style>
-</style>
